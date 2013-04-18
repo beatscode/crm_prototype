50a11a7bd2c798ea369b02a47b885100d5a86bad
Alex Casanova <alexcasanova2008@gmail.com>
2013-04-18 13:46:26 -0400

    added rails.env to templateswq!

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,5 +1,5 @@
 class Template < ActiveRecord::Base
-	establish_connection "templates_#{RAILS_ENV}"
+	establish_connection "templates_#{Rails.env}"
 	attr_accessible :name, :creator, :layout,
 					:path, :hidden, :approved,:description,
 					:created_at, :updated_at, :requested_approval_at
