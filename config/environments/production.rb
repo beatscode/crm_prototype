923ce4e4e2b64a865998b1ed3943825cb1056c2f
Alex Casanova <alexcasanova2008@gmail.com>
2013-05-03 18:45:23 -0400

    removed asset prefix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,4 @@ Leoadmin::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_controller.relative_url_root = "/leoadmin"
-  config.assets.prefix = "/leoadmin"
 end
