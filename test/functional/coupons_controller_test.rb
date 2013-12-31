619f1e14f334f6acd1fe4e393bceccf22e5caf11
Robbins Cleozier <rcleozier@gmail.com>
2013-12-31 11:55:15 -0500

    Finish coupons testing

--- test/functional/coupons_controller_test.rb
+++ test/functional/coupons_controller_test.rb
@@ -2,17 +2,58 @@ require 'test_helper'
 
 class CouponsControllerTest < ActionController::TestCase
 
-  test "should get index" do
+  test "Should get coupon index page" do
     session[:username] = "admin"
     get :index
     assert_response :success
   end
 
-  test "should get new" do
-    skip "need to rewrite"
+  test "Should get new coupon page" do
     session[:username] = "admin"
     get :new
     assert_response :success
+  end
+
+  test "should test creating a new coupon" do
+    session[:username] = "admin"
+    
+    post :create, { :coupon =>
+                        {:name => 'test coupon 2', 
+                         :code => 'testing2',
+                         :discount => 20,
+                         :discount_type => 1,
+                         :start_date => '',
+                         :end_date => ''
+                          }
+                      }
+    assert_response :success
+  end
+
+  test "Deleting a coupon" do
+    session[:username] = "admin"
+    post :delete, {:id => 1}
+    assert_response :redirect  
+  end
+
+  test "Loading coupon edit page" do
+    session[:username] = "admin"
+    get :edit, {:id => 1}
+    assert_response :success  
+  end
+
+  test "Updating a coupon" do
+    session[:username] = "admin"
+    post :updateCoupon, { :coupon =>
+                        {:id => 1,
+                          :name => 'test coupon 2', 
+                         :code => 'testing2',
+                         :discount => 20,
+                         :discount_type => 1,
+                         :start_date => '',
+                         :end_date => ''
+                          }
+                      }
+    assert_response :redirect
   end
 
 
