307c54faafcae3d28e595f40a27324a2dbfc50dd
Robbins Cleozier <rcleozier@gmail.com>
2013-12-31 13:11:22 -0500

    Created products and login test

--- test/functional/login_controller_test.rb
+++ test/functional/login_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class LoginControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "Should test logging in with correct credintial" do
+   post :complete, {"username" => AdminUsers::LOGIN[:username], "password" => AdminUsers::LOGIN[:password] }
+   assert_response :redirect	
+  end
+
+  test "Should test logging in with incorrect credintial" do
+   post :complete, {"username" => "Badusername", "password" => "passaos" }
+   assert_response :success
+  end
+
+
 end
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -1,7 +1,57 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   
+   test "Should get index" do
+   	session[:username] = "admin"
+    get :index
+    assert_not_nil assigns(:products)
+    assert_response :success
+  end
+
+  test "Should get edit product page" do
+   	session[:username] = "admin"
+    get :edit, {"id" => 1}
+    assert_not_nil assigns(:product)
+    assert_response :success
+  end
+
+  test "Should update a product" do
+   	session[:username] = "admin"
+    post :updateProduct,{ :product =>
+                      
+                        	{:id => 1,
+                        	 :name => 'Test',
+                        	 :price => 35.00,
+                        	 :product_type => 'addon' }
+                     	
+                     	}
+    assert_not_nil assigns(:product)
+    assert_response :redirect
+  end
+
+  test "Should load new products page" do
+   	session[:username] = "admin"
+    get :new
+    assert_not_nil assigns(:product)
+    assert_response :success
+  end
+
+
+  test "Should test creating a product" do
+   	session[:username] = "admin"
+    post :create,{ :product =>
+                      
+                        	{:id => 1,
+                        	 :name => 'Test',
+                        	 :price => 35.00,
+                        	 :product_type => 'addon',
+                        	 :recurring => 'yes',
+                        	 :recurring_amount => 20.00 }
+                     	
+                     	}
+    assert_not_nil assigns(:product)
+    assert_response :redirect
+  end
+
 end
