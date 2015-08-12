38ddc5958720ce5f5b23b490d116de6f05526b87
Alexander Casanova <alexcasanova2008@gmail.com>
2015-08-12 13:13:40 -0400

    removed config directory

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Leoadmin::Application.config.secret_token = '196643b1b796cf4e65a6a1cfda166cb03423a3b99e7117ac3a6d6f6e16d69333998418f957caa511960c882c7431d5f78aa4c00821ec875bdf51cc046713c912'
